e6781bc5587f3b9afb77141b1ebf41f3f5baa01c
DINMAY KUMAR BRAHMA <131891590+dino65-dev@users.noreply.github.com>
2024-11-27 00:52:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from huggingface_hub import InferenceClient
+import streamlit as st
+
+client = InferenceClient(
+    "stabilityai/stable-diffusion-3.5-large",
+    token="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"
+)
+
+user_input = st.text_input("Enter your image prompt here")
+st.title("Stable Diffusion 3.5 Large")
+submit = st.button("Generate Image")
+# output is a PIL.Image object
+if submit:
+    image = client.text_to_image(f"{user_input}")
+    st.image(image, caption=f"{user_input}")
